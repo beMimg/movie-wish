b73add7181e78ffadcf78d10ca92e80528d2561b
beMimg <bernardommguerreiro@gmail.com>
2024-02-06 11:17:59 +0100

    genre data

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import cors from "cors";
 import axios from "axios";
 import "dotenv/config";
 
+const genreIDS = [
+  28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770,
+  53, 10752, 37,
+];
+
 const app = express();
 
 app.use(cors());
@@ -75,7 +80,30 @@ app.get("/list", (req, res) => {
   fetch("https://api.themoviedb.org/3/genre/movie/list", options)
     .then((response) => response.json())
     .then((data) => res.json(data))
+
     .catch((err) => console.error(err));
 });
+
+genreIDS.forEach((id) => getGenre(id));
+
+export function getGenre(id) {
+  app.get(`/genre/${id}`, (req, res) => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: process.env.REACT_APP_API_KEY,
+      },
+    };
+
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?&with_genres=${id}`,
+      options,
+    )
+      .then((response) => response.json())
+      .then((data) => res.json(data))
+      .catch((err) => console.error(err));
+  });
+}
 
 app.listen(8000, () => console.log(`Server is running on port ${PORT}`));
