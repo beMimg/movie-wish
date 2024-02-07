d777f7f13bb3619c21caef0ab0b1563fbf2e045f
beMimg <bernardommguerreiro@gmail.com>
2024-02-07 17:46:11 +0100

    menu ui improved

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,9 +4,12 @@ import { IoIosClose } from "react-icons/io";
 export default function Menu({ open, setOpen }) {
   return (
     <div className="absolute left-0 top-0 z-30 h-screen w-full  bg-black bg-opacity-40 md:hidden">
-      <div className="flex h-full w-2/3 flex-col gap-4 bg-black p-8">
-        <IoIosClose onClick={() => setOpen(!open)} />
-        <NavegationLinks></NavegationLinks>
+      <div className="relative flex h-full w-2/4 flex-col gap-4 bg-black p-8">
+        <IoIosClose
+          onClick={() => setOpen(!open)}
+          className="grid self-end text-6xl"
+        />
+        <NavegationLinks setOpen={setOpen} open={open}></NavegationLinks>
       </div>
     </div>
   );
--- src/components/NavegationLinks.jsx
+++ src/components/NavegationLinks.jsx
@@ -1,11 +1,46 @@
 import { NavLink } from "react-router-dom";
+import { IoIosHeart } from "react-icons/io";
+import { TbCategoryFilled } from "react-icons/tb";
+import { IoIosHome } from "react-icons/io";
+import { IoIosInformation } from "react-icons/io";
+import { IoExtensionPuzzle } from "react-icons/io5";
 
-export default function NavegationLinks() {
+export default function NavegationLinks({ open, setOpen }) {
+  const activeLink =
+    "flex items-center justify-center gap-2 p-4 pl-16 md:pl-0 md:text-yellow-300 text-2xl ";
+  const normalLink = "flex items-center gap-2 p-4 md:text-xl";
   return (
     <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="popular">Popular</NavLink>
-      <NavLink to="categories">Categories</NavLink>
+      <NavLink
+        to="/"
+        onClick={() => setOpen(!open)}
+        className={({ isActive }) => (isActive ? activeLink : normalLink)}
+      >
+        <IoIosHome />
+        Home
+      </NavLink>
+      <NavLink
+        to="categories"
+        onClick={() => setOpen(!open)}
+        className={({ isActive }) => (isActive ? activeLink : normalLink)}
+      >
+        <TbCategoryFilled /> Categories
+      </NavLink>
+      <NavLink
+        to="wish_list"
+        onClick={() => setOpen(!open)}
+        className={({ isActive }) => (isActive ? activeLink : normalLink)}
+      >
+        <IoIosHeart /> Wish list
+      </NavLink>
+      <NavLink
+        to="about_us"
+        onClick={() => setOpen(!open)}
+        className={({ isActive }) => (isActive ? activeLink : normalLink)}
+      >
+        <IoExtensionPuzzle />
+        About us
+      </NavLink>
     </>
   );
 }
